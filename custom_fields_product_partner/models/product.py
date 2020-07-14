779aae5d42197456c72709d9f248dbdbb0bc87d7
thangaraj.m <thangaraj.murugesan@pptservices.com>
2020-07-14 14:15:48 +0530

    product changes updated

--- custom_fields_product_partner/models/product.py
+++ custom_fields_product_partner/models/product.py
@@ -21,9 +21,9 @@ class ProductTemplate(models.Model):
 #     x_studio_field_CCkqP = fields.Char('Storage Location (text)')
     x_studio_field_TVZyx = fields.Integer('')
     x_studio_field_jXS3W = fields.Many2one('uom.uom',string='Sale Unit of Measure - Reference ONLY')
-    x_studio_field_qr3ai = fields.Char('MPN')
+    x_studio_field_qr3ai = fields.Char('MPN/Customer/Supplier Part No')
     x_studio_field_pFxVK = fields.Char('Customer | Supplier Part Number (Search Key 1)')
-    x_studio_field_mHzKJ = fields.Char('Description (Search Key 2)')
+    x_studio_field_mHzKJ = fields.Char('Description')
     x_studio_field_ZhMOw = fields.Text('Description')
 
 class Manufacturer(models.Model):
