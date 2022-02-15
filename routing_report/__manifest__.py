fcd053f76f2daf5ef599e031de5a37ab50b10e38
keus-odoo <keus-odoo>
2022-02-15 14:51:21 +0530

    Routing report enabled

---
+++ routing_report/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Routing Report',
+    'version': '12.0',
+    'author': 'PPTS [India] Pvt.Ltd.',
+    'website': 'https://www.pptssolutions.com',
+    'category': 'MRP',
+    'description': """Routing Order PDF Report""",
+    'depends': ['mrp', 'stock', 'web'],
+    'data': [
+        'report/routing_report.xml',
+        'report/routing_report_templates.xml',
+        'report/layouts.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
