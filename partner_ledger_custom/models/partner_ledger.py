f29fbfe7c7104f02f6cd7f978b4cdad420577a7d
keus-odoo <keus-odoo>
2022-03-23 13:32:22 +0530

    issue fixing3

--- partner_ledger_custom/models/partner_ledger.py
+++ partner_ledger_custom/models/partner_ledger.py
@@ -124,7 +124,86 @@ class AccountPartnerLedgerReportInh(models.AbstractModel):
             'caret_options': caret_type,
             'level': 4,
         }
-        
+    
+    @api.model
+    def _get_partner_ledger_lines(self, options, line_id=None):
+        ''' Get lines for the whole report or for a specific line.
+        :param options: The report options.
+        :return:        A list of lines, each one represented by a dictionary.
+        '''
+        lines = []
+        unfold_all = options.get('unfold_all') or (self._context.get('print_mode') and not options['unfolded_lines'])
+
+        expanded_partner = line_id and self.env['res.partner'].browse(int(line_id[8:]))
+        partners_results = self._do_query(options, expanded_partner=expanded_partner)
+
+        _logger.info('partners_results -------partners_results------- %s' % partners_results)
+
+        total_initial_balance = total_debit = total_credit = total_balance = 0.0
+        for partner, results in partners_results:
+
+            _logger.info('partner -------partner------partner- %s' % partner)
+            
+            is_unfolded = 'partner_%s' % (partner.id if partner else 0) in options['unfolded_lines']
+
+            # res.partner record line.
+            partner_sum = results.get('sum', {})
+            partner_init_bal = results.get('initial_balance', {})
+
+            initial_balance = partner_init_bal.get('balance', 0.0)
+            debit = partner_sum.get('debit', 0.0)
+            credit = partner_sum.get('credit', 0.0)
+            balance = initial_balance + partner_sum.get('balance', 0.0)
+
+            lines.append(self._get_report_line_partner(options, partner, initial_balance, debit, credit, balance))
+
+            total_initial_balance += initial_balance
+            total_debit += debit
+            total_credit += credit
+            total_balance += balance
+
+            if unfold_all or is_unfolded:
+                cumulated_balance = initial_balance
+
+                # account.move.line record lines.
+                amls = results.get('lines', [])
+
+                load_more_remaining = len(amls)
+                load_more_counter = self._context.get('print_mode') and load_more_remaining or self.MAX_LINES
+
+                for aml in amls:
+                    # Don't show more line than load_more_counter.
+                    if load_more_counter == 0:
+                        break
+
+                    cumulated_init_balance = cumulated_balance
+                    cumulated_balance += aml['balance']
+                    lines.append(self._get_report_line_move_line(options, partner, aml, cumulated_init_balance, cumulated_balance))
+
+                    load_more_remaining -= 1
+                    load_more_counter -= 1
+
+                if load_more_remaining > 0:
+                    # Load more line.
+                    lines.append(self._get_report_line_load_more(
+                        options,
+                        partner,
+                        self.MAX_LINES,
+                        load_more_remaining,
+                        cumulated_balance,
+                    ))
+
+        if not line_id:
+            # Report total line.
+            lines.append(self._get_report_line_total(
+                options,
+                total_initial_balance,
+                total_debit,
+                total_credit,
+                total_balance
+            ))
+        return lines
+
     @api.model
     def _get_report_line_partner(self, options, partner, initial_balance, debit, credit, balance):
         company_currency = self.env.company.currency_id
