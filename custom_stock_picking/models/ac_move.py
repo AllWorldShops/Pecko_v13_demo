4770d594e5f0ada1e77d7199fea3183d25dae450
keus-odoo <keus-odoo>
2022-03-29 15:08:49 +0530

    journal report added1

--- custom_stock_picking/models/ac_move.py
+++ custom_stock_picking/models/ac_move.py
@@ -1,11 +1,16 @@
 from odoo import models, fields, api, _
 from datetime import date
+from odoo.exceptions import ValidationError, Warning
+
 
 class AcmoveInherit(models.Model):
     _inherit = 'account.move'
     
-    receipts_id = fields.Many2one('stock.picking', string="Receipts", store=False, domain=[('state', '=', 'done'),('picking_type_id.code', '=', 'incoming')])
-    
+    receipts_id = fields.Many2one('stock.picking', string="Receipts",
+    readonly=True, states={'draft': [('readonly', False)]})
+    picking_ids = fields.Many2many('stock.picking', string="Picking_ids")
+    # amount_untaxed = fields.Float(string='Untaxed Amount', store=True, readonly=True, tracking=True,
+    #     compute='_compute_amount' ,digits='Product Price')
     @api.onchange('partner_id')
     def onchange_partner_id(self):
         res = super(AcmoveInherit, self)._onchange_partner_id()
@@ -13,6 +18,7 @@ class AcmoveInherit(models.Model):
             # if rec.partner_id:
             return {'domain': {'receipts_id':[('partner_id', '=', rec.partner_id.id),('state', '=', 'done'),('picking_type_id.code', '=', 'incoming')]}}
         return res
+
     
     @api.onchange('receipts_id')
     def onchange_receipts_id(self):
@@ -20,35 +26,46 @@ class AcmoveInherit(models.Model):
         currency = self.currency_id
         company_currency = company.currency_id
         date = self.date or fields.Date.context_today(self)
+        id_list = []
         if self.receipts_id:
             receipt_lines = []
-            for rec in self:
-                account_id = self.env['account.account'].sudo().search([('deprecated', '=', False),('user_type_id.type', 'not in', ['receivable', 'payable']),('company_id','=',self.company_id.id)],limit=1)
-                for line in self.receipts_id.move_ids_without_package:
-                    product_price_unit = line.purchase_line_id.price_unit
-                    if currency and currency != company_currency:
-                        product_price_unit = company_currency._convert(line.purchase_line_id.price_unit, currency, company, date)
-                    print(product_price_unit, "product_price_unittt")
-                    val = {
+            account_id = self.env['account.account'].sudo().search([('deprecated', '=', False),('user_type_id.type', 'not in', ['receivable', 'payable']),('company_id','=',self.company_id.id)],limit=1)
+            for line in self.receipts_id.move_ids_without_package:
+                product_price_unit = line.purchase_line_id.price_unit
+                if currency and currency != company_currency:
+                    product_price_unit = company_currency._convert(line.purchase_line_id.price_unit, currency, company, date)
+                val = {
                     'product_id':line.product_id.id,
                     'quantity':line.quantity_done,
-                    'price_unit': product_price_unit,
-                    'account_id': account_id.id if account_id else False,
+                    'price_unit': line.purchase_line_id.price_unit if line.purchase_line_id else line.product_id.standard_price,
+                    'account_id': False,
                     'name':line.product_id.name,
                     'tax_ids': line.purchase_line_id.taxes_id.ids,
                     'product_uom_id': line.product_uom.id if line.product_uom else line.product_id.uom_id.id,
+                    'st_move_id': line.id,
                     # 'price_subtotal': line.quantity_done * product_price_unit,
-                    }
-                    receipt_lines.append((0, 0, val))
-                    if not rec.invoice_line_ids:
-                        rec.invoice_line_ids = receipt_lines
-                        self._onchange_currency()
-                    else:
-                        for i_line in rec.invoice_line_ids:
-                            i_line.update(val)
-                            i_line.move_id._onchange_currency()
+                }
+                receipt_lines.append((0, 0, val))
 
+            id_list.append(self.receipts_id.id)
+            # if not self.invoice_line_ids:
+            # self.receipts_id = False
+            
+            for rec in self:
+                if rec.receipts_id.id in rec.picking_ids.ids:
+                    pass
+                    # raise Warning("This Receipt has been already selected !")
+                else:
+                    rec.picking_ids = [(4, x, None) for x in id_list]
+                    self.invoice_line_ids = receipt_lines
+                    rec._onchange_currency()
+                for i_line in rec.invoice_line_ids:
+                    i_line.account_id = i_line._get_computed_account()
+                
+class AcMoveLine(models.Model):
+    _inherit = 'account.move.line'
 
+    st_move_id = fields.Many2one('stock.move')
         
             
         
