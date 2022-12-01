6a61b80eea9c5f0fc0443ac9caf426e20837a41b
thangaraj.m <thangaraj.murugesan@pptservices.com>
2022-12-01 12:22:58 +0530

    Detrack integration enabled1

--- mrp_customization/models/mrp_configuration.py
+++ mrp_customization/models/mrp_configuration.py
@@ -2,7 +2,9 @@ from odoo import fields, models, api
 
 class UrlConfig(models.Model):
     _name = "url.config"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "url"
     
-    name = fields.Char(string='URL',required=True)
-    code = fields.Char(string='Code',required=True)
+    name = fields.Char(string='URL',required=True, tracking=True)
+    code = fields.Char(string='Code',required=True, tracking=True)
+    active = fields.Boolean("Active", default=True)
