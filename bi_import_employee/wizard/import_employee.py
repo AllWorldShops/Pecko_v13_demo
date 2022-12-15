f5735efb1a35c992a2e24e053da50a4beef0a406
vimalprasad37 <vimalprasad37@gmail.com>
2022-12-15 19:12:28 +0530

    Product routes updation via query1

--- bi_import_employee/wizard/import_employee.py
+++ bi_import_employee/wizard/import_employee.py
@@ -43,10 +43,7 @@ class ImportProducts(models.TransientModel):
 			raise ValidationError(_("Please Upload File to Import Products !"))
 
 		if self.file_type == 'CSV':
-			# line = keys = ['default_code', 'uom_id', 'name', 'x_studio_field_qr3ai','x_studio_field_mHzKJ',
-            #       'standard_price','min_qty','pname','uom_po_id','currency_id',
-            #       'price', 'delay', 'route_ids', 'categ_id']
-			line = keys = ['name', 'order_seq']
+			line = keys = ['product_id']
 			try:
 				csv_data = base64.b64decode(self.file)
 				data_file = io.StringIO(csv_data.decode("utf-8"))
@@ -65,8 +62,7 @@ class ImportProducts(models.TransientModel):
 					if i == 0:
 						continue
 					else:
-						# res = self.create_products(values)
-						res = self.update_record(values)
+						res = self.create_products(values)
 		else:
 			try:
 				file = tempfile.NamedTemporaryFile(delete= False,suffix=".xlsx")
@@ -84,28 +80,81 @@ class ImportProducts(models.TransientModel):
 					fields = list(map(lambda row:row.value.encode('utf-8'), sheet.row(row_no)))
 				else:
 					line = list(map(lambda row:isinstance(row.value, bytes) and row.value.encode('utf-8') or str(row.value), sheet.row(row_no)))
-					# values.update( {
-					# 		# 'name':line[0],
-					# 		# 'job_title': line[1],,
-					# 		'default_code': line[0],
-       				# 		'uom_id': line[1],
-					# 		'name': line[2],
-       				# 		'x_studio_field_qr3ai': line[3],
-             		# 		'x_studio_field_mHzKJ': line[4],
-                  	# 		'standard_price': line[5],
-                  	# 		'min_qty':line[6],
-					# 		'pname':line[7],
-       				# 		'uom_po_id':line[8],
-             		# 		'currency_id':line[9],
-                  	# 		'price': line[10],
-                    #  		'delay': line[11],
-                    #    		'route_ids': line[12],
-                    #      	'categ_id': line[13]
-					# 		})
-					values.update({'name': line[0],
-								'order_seq': line[1]})
-					# res = self.create_products(values)
-					res = self.update_record(values)
+					product_id = line[0]
+					print(product_id, "product_id------")
+					update_routes = """
+					    insert into stock_route_product (route_id,product_id) values (25, %s);
+					    """ % (product_id)
+					self.env.cr.execute(update_routes)
+
+	# def import_products(self):
+	# 	if not self.file:
+	# 		raise ValidationError(_("Please Upload File to Import Products !"))
+
+	# 	if self.file_type == 'CSV':
+	# 		# line = keys = ['default_code', 'uom_id', 'name', 'x_studio_field_qr3ai','x_studio_field_mHzKJ',
+    #         #       'standard_price','min_qty','pname','uom_po_id','currency_id',
+    #         #       'price', 'delay', 'route_ids', 'categ_id']
+	# 		line = keys = ['name', 'order_seq']
+	# 		try:
+	# 			csv_data = base64.b64decode(self.file)
+	# 			data_file = io.StringIO(csv_data.decode("utf-8"))
+	# 			data_file.seek(0)
+	# 			file_reader = []
+	# 			csv_reader = csv.reader(data_file, delimiter=',')
+	# 			file_reader.extend(csv_reader)
+	# 		except Exception:
+	# 			raise ValidationError(_("Please Select Valid File Format !"))
+				
+	# 		values = {}
+	# 		for i in range(len(file_reader)):
+	# 			field = list(map(str, file_reader[i]))
+	# 			values = dict(zip(keys, field))
+	# 			if values:
+	# 				if i == 0:
+	# 					continue
+	# 				else:
+	# 					# res = self.create_products(values)
+	# 					res = self.update_record(values)
+	# 	else:
+	# 		try:
+	# 			file = tempfile.NamedTemporaryFile(delete= False,suffix=".xlsx")
+	# 			file.write(binascii.a2b_base64(self.file))
+	# 			file.seek(0)
+	# 			values = {}
+	# 			workbook = xlrd.open_workbook(file.name)
+	# 			sheet = workbook.sheet_by_index(0)
+	# 		except Exception:
+	# 			raise ValidationError(_("Please Select Valid File Format !"))
+
+	# 		for row_no in range(sheet.nrows):
+	# 			val = {}
+	# 			if row_no <= 0:
+	# 				fields = list(map(lambda row:row.value.encode('utf-8'), sheet.row(row_no)))
+	# 			else:
+	# 				line = list(map(lambda row:isinstance(row.value, bytes) and row.value.encode('utf-8') or str(row.value), sheet.row(row_no)))
+	# 				# values.update( {
+	# 				# 		# 'name':line[0],
+	# 				# 		# 'job_title': line[1],,
+	# 				# 		'default_code': line[0],
+    #    				# 		'uom_id': line[1],
+	# 				# 		'name': line[2],
+    #    				# 		'x_studio_field_qr3ai': line[3],
+    #          		# 		'x_studio_field_mHzKJ': line[4],
+    #               	# 		'standard_price': line[5],
+    #               	# 		'min_qty':line[6],
+	# 				# 		'pname':line[7],
+    #    				# 		'uom_po_id':line[8],
+    #          		# 		'currency_id':line[9],
+    #               	# 		'price': line[10],
+    #                 #  		'delay': line[11],
+    #                 #    		'route_ids': line[12],
+    #                 #      	'categ_id': line[13]
+	# 				# 		})
+	# 				values.update({'name': line[0],
+	# 							'order_seq': line[1]})
+	# 				# res = self.create_products(values)
+	# 				res = self.update_record(values)
 
 
 	def update_record(self, values):
