5a4e0e463583a51a5518ccf29ad393d232513543
vimalprasad37 <vimalprasad37@gmail.com>
2023-01-05 15:20:05 +0530

    Location domain updated

--- custom_stock_picking/models/stock.py
+++ custom_stock_picking/models/stock.py
@@ -81,23 +81,31 @@ class StockInventory(models.Model):
 class StockInventoryLine(models.Model):
     _inherit = 'stock.inventory.line'
 
-    def _check_no_duplicate_line(self):
-        for line in self:
-            domain = [
-                ('id', '!=', line.id),
-                ('product_id', '=', line.product_id.id),
-                ('location_id', '=', line.location_id.id),
-                ('partner_id', '=', line.partner_id.id),
-                ('package_id', '=', line.package_id.id),
-                ('prod_lot_id', '=', line.prod_lot_id.id),
-                ('inventory_id', '=', line.inventory_id.id)]
-            if line.location_id.usage != 'internal':
-                dmn = self.search(domain)
-                _logger.info("-------Duplicate Lineitem : %s------" % dmn)
-            existings = self.search_count(domain)
-            if existings:
-                raise UserError(_("There is already one inventory adjustment line for this product,"
-                                  " you should rather modify this one instead of creating a new one."))
+    @api.model
+    def _domain_location_id(self):
+        if self.env.context.get('active_model') == 'stock.inventory':
+            inventory = self.env['stock.inventory'].browse(self.env.context.get('active_id'))
+            if inventory.exists() and inventory.location_ids:
+                return "[('company_id', '=', company_id), ('id', 'child_of', %s)]" % inventory.location_ids.ids
+        return "[('company_id', '=', company_id)]"
+
+    # def _check_no_duplicate_line(self):
+    #     for line in self:
+    #         domain = [
+    #             ('id', '!=', line.id),
+    #             ('product_id', '=', line.product_id.id),
+    #             ('location_id', '=', line.location_id.id),
+    #             ('partner_id', '=', line.partner_id.id),
+    #             ('package_id', '=', line.package_id.id),
+    #             ('prod_lot_id', '=', line.prod_lot_id.id),
+    #             ('inventory_id', '=', line.inventory_id.id)]
+    #         if line.location_id.usage != 'internal':
+    #             dmn = self.search(domain)
+    #             _logger.info("-------Duplicate Lineitem : %s------" % dmn)
+    #         existings = self.search_count(domain)
+    #         if existings:
+    #             raise UserError(_("There is already one inventory adjustment line for this product,"
+    #                               " you should rather modify this one instead of creating a new one."))
 
 
 class ProductTemplate(models.Model):
