d1675d5bb95936945268cb92f69be3ff98299fc5
vimalprasad37 <vimalprasad37@gmail.com>
2023-01-07 16:13:26 +0530

    Location domain updated

--- custom_stock_picking/models/stock.py
+++ custom_stock_picking/models/stock.py
@@ -89,23 +89,25 @@ class StockInventoryLine(models.Model):
                 return "[('company_id', '=', company_id), ('id', 'child_of', %s)]" % inventory.location_ids.ids
         return "[('company_id', '=', company_id)]"
 
-    # def _check_no_duplicate_line(self):
-    #     for line in self:
-    #         domain = [
-    #             ('id', '!=', line.id),
-    #             ('product_id', '=', line.product_id.id),
-    #             ('location_id', '=', line.location_id.id),
-    #             ('partner_id', '=', line.partner_id.id),
-    #             ('package_id', '=', line.package_id.id),
-    #             ('prod_lot_id', '=', line.prod_lot_id.id),
-    #             ('inventory_id', '=', line.inventory_id.id)]
-    #         if line.location_id.usage != 'internal':
-    #             dmn = self.search(domain)
-    #             _logger.info("-------Duplicate Lineitem : %s------" % dmn)
-    #         existings = self.search_count(domain)
-    #         if existings:
-    #             raise UserError(_("There is already one inventory adjustment line for this product,"
-    #                               " you should rather modify this one instead of creating a new one."))
+    def _check_no_duplicate_line(self):
+        for line in self:
+            domain = [
+                ('id', '!=', line.id),
+                ('product_id', '=', line.product_id.id),
+                ('location_id', '=', line.location_id.id),
+                ('partner_id', '=', line.partner_id.id),
+                ('package_id', '=', line.package_id.id),
+                ('prod_lot_id', '=', line.prod_lot_id.id),
+                ('inventory_id', '=', line.inventory_id.id)
+                ('state', '!=', 'done')]
+            # if line.location_id.usage != 'internal':
+            #     dmn = self.search(domain)
+            #     _logger.info("-------Duplicate Lineitem : %s------" % dmn)
+            #     dmn.unlink()
+            existings = self.search_count(domain)
+            if existings:
+                raise UserError(_("There is already one inventory adjustment line for this product,"
+                                  " you should rather modify this one instead of creating a new one."))
 
 
 class ProductTemplate(models.Model):
