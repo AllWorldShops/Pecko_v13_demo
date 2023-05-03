e93b58a388f7fd22dd26ee399b77fbca84e80116
thangaraj.m <thangaraj.murugesan@pptservices.com>
2023-05-03 20:52:27 +0530

    queue added4

--- stock_mts_mto_rule/models/stock_rule.py
+++ stock_mts_mto_rule/models/stock_rule.py
@@ -6,7 +6,6 @@ from odoo.osv import expression
 from odoo.tools import float_compare, float_is_zero
 
 from collections import defaultdict
-from odoo import api, fields, models, SUPERUSER_ID, _
 
 
 class StockRule(models.Model):
@@ -113,40 +112,32 @@ class StockRule(models.Model):
                 )
         return True
 
-
     @api.model
     def _run_manufacture(self, procurements):
         productions_values_by_company = defaultdict(list)
         for procurement, rule in procurements:
-            if float_compare(procurement.product_qty, 0, precision_rounding=procurement.product_uom.rounding) <= 0:
-                # If procurement contains negative quantity, don't create a MO that would be for a negative value.
-                continue
-            bom = rule._get_matching_bom(procurement.product_id, procurement.company_id, procurement.values)
+            bom = self._get_matching_bom(procurement.product_id, procurement.company_id, procurement.values)
+            if not bom:
+                msg = _('There is no Bill of Material of type manufacture or kit found for the product %s. Please define a Bill of Material for this product.') % (procurement.product_id.display_name,)
+                raise UserError(msg)
 
             productions_values_by_company[procurement.company_id.id].append(rule._prepare_mo_vals(*procurement, bom))
 
         for company_id, productions_values in productions_values_by_company.items():
             # create the MO as SUPERUSER because the current user may not have the rights to do it (mto product launched by a sale for example)
-            productions = self.env['mrp.production'].with_user(SUPERUSER_ID).sudo().with_company(company_id).with_delay().create(productions_values)
-            productions.filtered(lambda p: (not p.orderpoint_id and p.move_raw_ids) or\
-                (p.move_dest_ids.procure_method != 'make_to_order' and not p.move_raw_ids and not p.workorder_ids)).action_confirm()
+            productions = self.env['mrp.production'].sudo().with_context(force_company=company_id).with_delay().create(productions_values)
+            self.env['stock.move'].sudo().create(productions._get_moves_raw_values())
+            productions.action_confirm()
 
             for production in productions:
                 origin_production = production.move_dest_ids and production.move_dest_ids[0].raw_material_production_id or False
                 orderpoint = production.orderpoint_id
-                if orderpoint and orderpoint.create_uid.id == SUPERUSER_ID and orderpoint.trigger == 'manual':
-                    production.message_post(
-                        body=_('This production order has been created from Replenishment Report.'),
-                        message_type='comment',
-                        subtype_xmlid='mail.mt_note')
-                elif orderpoint:
-                    production.message_post_with_view(
-                        'mail.message_origin_link',
-                        values={'self': production, 'origin': orderpoint},
-                        subtype_id=self.env.ref('mail.mt_note').id)
-                elif origin_production:
-                    production.message_post_with_view(
-                        'mail.message_origin_link',
-                        values={'self': production, 'origin': origin_production},
-                        subtype_id=self.env.ref('mail.mt_note').id)
+                if orderpoint:
+                    production.message_post_with_view('mail.message_origin_link',
+                                                      values={'self': production, 'origin': orderpoint},
+                                                      subtype_id=self.env.ref('mail.mt_note').id)
+                if origin_production:
+                    production.message_post_with_view('mail.message_origin_link',
+                                                      values={'self': production, 'origin': origin_production},
+                                                      subtype_id=self.env.ref('mail.mt_note').id)
         return True
