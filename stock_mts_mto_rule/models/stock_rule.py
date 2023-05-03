24d58b277d817ed486e54c5ed2e48e9ddbaca070
thangaraj.m <thangaraj.murugesan@pptservices.com>
2023-05-03 20:45:15 +0530

    queue added1

--- stock_mts_mto_rule/models/stock_rule.py
+++ stock_mts_mto_rule/models/stock_rule.py
@@ -5,6 +5,13 @@ from odoo.exceptions import ValidationError
 from odoo.osv import expression
 from odoo.tools import float_compare, float_is_zero
 
+from collections import defaultdict
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo.osv import expression
+from odoo.addons.stock.models.stock_rule import ProcurementException
+from odoo.tools import float_compare, OrderedSet
 
 class StockRule(models.Model):
     _inherit = "stock.rule"
