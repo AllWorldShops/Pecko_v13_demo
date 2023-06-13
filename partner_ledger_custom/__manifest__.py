20a67eb9cdb473ece74d08f3ceefe172bca137b2
thangaraj.m <thangaraj.murugesan@pptservices.com>
2023-06-13 16:06:47 +0530

    Partner ledger report issue fixed

--- partner_ledger_custom/__manifest__.py
+++ partner_ledger_custom/__manifest__.py
@@ -3,14 +3,17 @@
 
 {
     'name': 'Partner Ledger Custom',
-    'version': '13.0',
+    'version': '16.0',
     'summary': 'Partner Ledger',
     # 'description': 'This module is to round the invoice (decimal amount) to the nearest value for Customer Invoice and Vendor Bills',
     'category': 'Accounting',
     'author': 'PPTS [India] Pvt.Ltd.',
     'website': "www.pptssolutions.com",
     'company': 'PPTS INDIA PVT LTD',
-    'depends': ['base', 'account','sale'],
+    'depends': ['base', 'account','sale', 'account_reports'],
+    'data': [
+        'views/partner_ledger.xml',
+    ],
     'installable': True,
     'auto_install': False,
     'application': False,
