6e86650583add15e761ddaf78772127f5291a013
thangaraj.m <thangaraj.murugesan@pptservices.com>
2023-07-20 15:39:56 +0530

    BOM report changes

--- custom_mrp/__manifest__.py
+++ custom_mrp/__manifest__.py
@@ -14,24 +14,15 @@
         'views/decimal_data.xml',
         'views/res_config_view.xml',
         'report/custom_mrp_inherit.xml',
-
-
-
-
     ],
     'installable': True,
     'application': True,
     'auto_install': False,
     "assets": {
         "web.assets_backend": [
-
         'custom_mrp/static/src/components/inherit_bom_overview_table/mrp_inherit_bom_overview_table.xml',
         'custom_mrp/static/src/components/inherit_bom_overview_table/mrp_custom_base.xml',
         'custom_mrp/static/src/components/inherit_bom_overview_table/mrp_bom_display_filter.js',
         ]
     },
-    #     'qweb': [
-    #         '/custom_mrp/static/src/components/inherit_bom_overview_table/mrp_inherit_bom_overview_table.xml',
-    # ],
-
 }
--- custom_mrp/static/src/components/inherit_bom_overview_table/mrp_bom_display_filter.js
+++ custom_mrp/static/src/components/inherit_bom_overview_table/mrp_bom_display_filter.js
@@ -1,34 +1,15 @@
-/** @odoo-module **/
-import { Dropdown } from "@web/core/dropdown/dropdown";
-import { DropdownItem } from "@web/core/dropdown/dropdown_item";
-import { BomOverviewDisplayFilter } from "@mrp/components/bom_overview_display_filter/mrp_bom_overview_display_filter";
-const { Component } = owl;
-import { patch } from "@web/core/utils/patch";
-console.log(BomOverviewDisplayFilter,'BomOverviewDisplayFilter');
+odoo.define('web.mrp_bom_display_filter', function (require) {
+"use strict";
 
-patch(BomOverviewDisplayFilter.prototype, "mrp_costs", {
-    setup() {
-        this._super.apply();
-        this.displayOptions = {
-            availabilities: this.env._t('Availabilities'),
-            leadTimes: this.env._t('Lead Times'),
-//            costs: ('Costs'),
-            operations: this.env._t('Operations'),
-        };
-    },
+const { BomOverviewComponent } = require ("@mrp/components/bom_overview/mrp_bom_overview");
+const { patch }  = require ("@web/core/utils/patch");
+const { Component, useState } = owl;
+console.log(BomOverviewComponent,'BomOverviewComponent');
+
+patch(BomOverviewComponent.prototype, 'mrp_costs',{
+   setup() {
+   this._super.apply();
+        this.state.showOptions.costs = false
+    }
+    });
 });
-BomOverviewDisplayFilter.props = {
-    showOptions: {
-        type: Object,
-        shape: {
-            availabilities: Boolean,
-//            costs: Boolean,
-            operations: Boolean,
-            leadTimes: Boolean,
-            uom: Boolean,
-            attachments: Boolean,
-        },
-    },
-    changeDisplay: Function,
-};
-
