4e5392512af1c3db02cc274d9857f07fbd5a235a
thangaraj.m <thangaraj.murugesan@pptservices.com>
2023-07-31 10:39:30 +0530

    MOQ enabled

---
+++ supplier_min_qty_purchase/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'PPTS: Minimum purchase quantity based on Vendor',
+    'version': '1.1.5',
+    'category': 'Purchase',
+    'summary': 'Quantity of "Buy" products will be based on minimum quantity for the vendor.',
+    'description': """ This module has customizations for Purchase order generated from Sales/Manufacturing. """,
+    "author": "PPTS [India] Pvt.Ltd.",
+    'website': 'www.pptssolutions.com',
+    'depends': ['base', 'purchase_stock'],
+    # 'data': [
+    # ],
+    'demo': [ ],
+    'installable'   : True,
+    'auto_install'  : False,
+    'license'       : 'LGPL-3',
+}
