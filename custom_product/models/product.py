c1ed971e14a5ce268e9e052bae39c807fb45fd9a
thangaraj.m <thangaraj.murugesan@pptservices.com>
2023-08-02 12:25:43 +0530

    Vendor RFQ issue fixed

--- custom_product/models/product.py
+++ custom_product/models/product.py
@@ -54,6 +54,37 @@ class ProductProduct(models.Model):
             product_id = self.env['product.product'].search([('id','=',self.id)])
             product_id.product_tmpl_id.manufacturer_id = vals.get('manufacturer_id')
         return rec
+
+    def _select_seller(self, partner_id=False, quantity=0.0, date=None, uom_id=False, params=False):
+        self.ensure_one()
+        if date is None:
+            date = fields.Date.context_today(self)
+        precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+
+        res = self.env['product.supplierinfo']
+        sellers = self._prepare_sellers(params)
+        sellers = sellers.filtered(lambda s: not s.company_id or s.company_id.id == self.env.company.id)
+        for seller in sellers:
+            # Set quantity in UoM of seller
+            quantity_uom_seller = quantity
+            if quantity_uom_seller and uom_id and uom_id != seller.product_uom:
+                quantity_uom_seller = uom_id._compute_quantity(quantity_uom_seller, seller.product_uom)
+
+            # if seller.date_start and seller.date_start > date:
+            #     continue
+            # if seller.date_end and seller.date_end < date:
+            #     continue
+            # if partner_id and seller.partner_id not in [partner_id, partner_id.parent_id]:
+            #     continue
+            # if quantity is not None and float_compare(quantity_uom_seller, seller.min_qty, precision_digits=precision) == -1:
+            #     continue
+            # if seller.product_id and seller.product_id != self:
+            #     continue
+
+            if not res or res.partner_id == seller.partner_id:
+                res |= seller
+
+        return res.sorted('sequence')[:1]
     
 class ResCompanyInh(models.Model):
     _inherit = 'res.company'
