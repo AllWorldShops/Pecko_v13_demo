f2d32a3a6bac43329098f5e6c30b909118c77fbf
thangaraj.m <thangaraj.murugesan@pptservices.com>
2023-08-04 11:35:26 +0530

    MOQ based RFQ creation enabled

---
+++ supplier_min_qty_purchase/models/purchase.py
@@ -0,0 +1,42 @@
+from odoo import api, fields, models, _
+
+
+class PurchaseorderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    @api.model
+    def _prepare_purchase_order_line_from_procurement(self, product_id, product_qty, product_uom, company_id, values, po):
+        line_description = ''
+        
+        if values.get('product_description_variants'):
+            line_description = values['product_description_variants']
+        supplier = values.get('supplier')
+
+        # customised by PPTS 
+        # Purpose:- The quantity of purchase order should be based on minimum quantity in Vendor/Supplier under 'products'.
+        ###############################################################################################
+            
+        if product_qty > supplier.min_qty:
+            qty = product_uom._compute_quantity(product_qty, product_id.uom_po_id)
+            sub_qty = (qty // supplier.min_qty) + 1
+            product_qty = supplier.min_qty * sub_qty
+        else:
+            qty = product_uom._compute_quantity(supplier.min_qty, product_id.uom_po_id)
+            product_qty = qty
+
+        if product_uom != product_id.uom_po_id:
+            product_uom = product_id.uom_po_id
+        ###############################################################################################
+
+        res = self._prepare_purchase_order_line(product_id, product_qty, product_uom, company_id, supplier, po)
+        # We need to keep the vendor name set in _prepare_purchase_order_line. To avoid redundancy
+        # in the line name, we add the line_description only if different from the product name.
+        # This way, we shoud not lose any valuable information.
+        if line_description and product_id.name != line_description:
+            res['name'] += '\n' + line_description
+        res['date_planned'] = values.get('date_planned')
+        res['move_dest_ids'] = [(4, x.id) for x in values.get('move_dest_ids', [])]
+        res['orderpoint_id'] = values.get('orderpoint_id', False) and values.get('orderpoint_id').id
+        res['propagate_cancel'] = values.get('propagate_cancel')
+        res['product_description_variants'] = values.get('product_description_variants')
+        return res
