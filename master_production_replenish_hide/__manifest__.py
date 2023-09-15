2afe66838893c3c3dc067d940c259f8913cc6cf4
thangaraj.m <thangaraj.murugesan@pptservices.com>
2023-09-15 14:28:38 +0530

    MPS replenish changes

---
+++ master_production_replenish_hide/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Master Production Replenish Hide',
+    'version': '1.0',
+    'category': 'Manufacturing/Manufacturing',
+    'sequence': 50,
+    'summary': 'Master Production Replenish Hide',
+    'depends': ['base_import', 'mrp', 'purchase_stock', 'mrp_mps', 'stock'],
+    'description': """
+Master Production Replenish Hide
+""",
+    'data': [
+
+        'views/res_company_views.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'master_production_replenish_hide/static/src/components/replenish_hide.xml',
+            'master_production_replenish_hide/static/src/components/replenish_line_view.xml',
+        ],
+
+    }
+}
