7aee93f0abd76cc7b6d325cbe0b68849c2c77b3f
thangaraj.m <thangaraj.murugesan@pptservices.com>
2023-11-08 10:57:28 +0530

    Excess qty issue resolved

--- picking_excess_qty_restriction/models/stock_picking.py
+++ picking_excess_qty_restriction/models/stock_picking.py
@@ -14,7 +14,7 @@ class StockPicking(models.Model):
         delivery_qty = self.env['ir.config_parameter'].sudo().get_param('delivery_quantity.excess_quantity_allow')
         if delivery_qty:
             for line in (self.move_ids_without_package.filtered
-                (lambda l: l.quantity_done > round(l.product_uom_qty,4) and self.picking_type_id.code in ['outgoing','incoming','internal','mrp_operation'])):
+                (lambda l: round(l.quantity_done,4) > l.product_uom_qty and self.picking_type_id.code in ['outgoing','incoming','internal','mrp_operation'])):
                     raise ValidationError(
                         _("Product level does not allow for more quantity to be delivered - '%s' Demand - '%s' Done - '%s'")
                         % (line.product_id.name,line.product_uom_qty,line.quantity_done))
