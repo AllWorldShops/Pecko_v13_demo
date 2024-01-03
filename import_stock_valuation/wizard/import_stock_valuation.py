7bdda5b6d271131855b3e49fc46b1420207cbc80
thangaraj.m <thangaraj.murugesan@pptservices.com>
2024-01-03 15:06:52 +0530

    stock valuation import added

---
+++ import_stock_valuation/wizard/import_stock_valuation.py
@@ -0,0 +1,38 @@
+import base64
+import csv
+from io import StringIO
+from odoo import models, fields, _
+from datetime import datetime
+
+
+class ImportStockValuation(models.TransientModel):
+    _name = "import.stock.valuation"
+
+    file = fields.Binary(string='File')
+
+    def import_stock_valuation_data(self):
+        csv_data = base64.b64decode(self.file)
+        csv_string = csv_data.decode('utf-8')
+        csv_reader = csv.DictReader(StringIO(csv_string))
+        for row in csv_reader:
+            if row:
+                create_date = row['Date']
+                stock_move = self.env['stock.move'].search([('reference', '=', row['Reference'])])
+                product = self.env['product.product'].search([('name', '=', row['Product'])])
+                if product:
+                    product_id = product
+                quantity = row['Done']
+                company_id = self.env['res.company'].search([('name', '=', row['Company'])])
+                unit_cost = product_id.standard_price
+                value = float(unit_cost) * float(quantity)
+                stock_valuation = self.env['stock.valuation.layer'].create({
+                    'create_date': create_date,
+                    'stock_move_id': stock_move.id or False,
+                    'product_id': product_id.id or False,
+                    'company_id': company_id.id or False,
+                    'quantity': quantity,
+                    'unit_cost': unit_cost,
+                    'value': value
+
+                })
+
