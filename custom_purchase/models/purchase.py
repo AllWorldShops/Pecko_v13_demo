ace1329af2c8a73cc9354b4ad550c8f35fd1bd72
thangaraj.m <thangaraj.murugesan@pptservices.com>
2024-02-06 18:42:18 +0530

    PO number generation issue fixed

--- custom_purchase/models/purchase.py
+++ custom_purchase/models/purchase.py
@@ -20,6 +20,26 @@ class PurchaseOrder(models.Model):
     
     old_po_no = fields.Char(string='Old PO Number')
 
+    @api.model_create_multi
+    def create(self, vals_list):
+        orders = self.browse()
+        partner_vals_list = []
+        for vals in vals_list:
+            company_id = vals.get('company_id', self.default_get(['company_id'])['company_id'])
+            # Ensures default picking type and currency are taken from the right company.
+            self_comp = self.with_company(company_id)
+            if vals.get('name', 'New') == 'New':
+                seq_date = fields.datetime.now()
+                # if 'date_order' in vals:
+                #     seq_date = fields.Datetime.context_timestamp(self, fields.Datetime.to_datetime(vals['date_order']))
+                vals['name'] = self_comp.env['ir.sequence'].next_by_code('purchase.order', sequence_date=seq_date) or '/'
+            vals, partner_vals = self._write_partner_values(vals)
+            partner_vals_list.append(partner_vals)
+            orders |= super(PurchaseOrder, self_comp).create(vals)
+        for order, partner_vals in zip(orders, partner_vals_list):
+            if partner_vals:
+                order.sudo().write(partner_vals)  # Because the purchase user doesn't have write on `res.partner`
+        return orders
 
     # passing the values customer_po_no field , purchase order to sale order)
     def _prepare_sale_order_data(self, name, partner, company, direct_delivery_address):
