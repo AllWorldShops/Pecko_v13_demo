2dbd372af21e887c39ea10339794d28d2e2730fc
Gowtham A <gowtham.a@pptssolutions.com>
2025-02-25 15:42:53 +0530

    Added the module for create rfq based on lead time

---
+++ custom_addons/stock_rule_custom/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'PPTS: Stock Rule Customizations',
+    'version': '18.0',
+    'category': 'Product',
+    'author': 'PPTS [India] Pvt.Ltd.',
+    'website': 'https://www.pptssolutions.com',
+    'summary': 'This module has customizations for the stock rule and Rfq and Mo automatic create based',
+    'description': """ This module has customizations for the stock rule. """,
+    "author": "PPTS [India] Pvt.Ltd.",
+    'website': 'www.pptssolutions.com',
+    'depends': ['base','stock', 'purchase_stock'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3'
+}
