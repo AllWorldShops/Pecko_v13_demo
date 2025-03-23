04a09309c2849c757192f5a2fb929db46620931a
Gowtham A <gowtham.a@pptssolutions.com>
2025-03-23 18:40:18 +0530

    layer import for raw material

--- import_stock_valuation/wizard/import_stock_valuation.py
+++ import_stock_valuation/wizard/import_stock_valuation.py
@@ -1,12 +1,12 @@
 import base64
 import csv
 from io import StringIO
-from docutils.nodes import reference
 from odoo import models, fields, _
 from datetime import datetime
 from odoo.exceptions import UserError, ValidationError
 from datetime import date
 import logging
+
 _logger = logging.getLogger(__name__)
 
 # Manually update the valuation and stock journal using xl sheet
@@ -16,161 +16,138 @@ class ImportStockValuation(models.TransientModel):
     file = fields.Binary(string='File')
 
     def import_stock_valuation_data(self):
+        if not self.file:
+            raise UserError("Please upload a valid CSV file.")
+
         csv_data = base64.b64decode(self.file)
         csv_string = csv_data.decode('utf-8')
         csv_reader = csv.DictReader(StringIO(csv_string))
         reference_count = 1
+        completed_count = 0
         date_end = datetime.strptime('2023-12-31', '%Y-%m-%d')
         date_start = datetime.strptime('2023-01-01', '%Y-%m-%d')
 
         for row in csv_reader:
-            if row:
-                create_date = datetime.strptime(row['Date'], '%Y-%m-%d')
-                product = self.env['product.product'].search(
-                    ['|', ('active', '=', True), ('active', '=', False), ('default_code', '=', row['Product'])], limit=1)
+            if not row.get('Date') or not row.get('Product'):
+                raise UserError("Missing required fields in CSV.")
 
-                if not product:
-                    raise UserError(_("Product not found: %s") % row['Product'])
+            create_date = datetime.strptime(row['Date'], '%Y-%m-%d')
+            product = self.env['product.product'].search([('default_code', '=', row['Product'])], limit=1)
 
-                existing_valuation = self.env['stock.valuation.layer'].search([
+            if not product.active:
+                _logger.warning("Active Product not found: %s", row['Product'])
+                continue
+            elif not product :
+                raise UserError(_("Product not found: %s") % row['Product'])
+
+
+            existing_valuation = self.env['stock.valuation.layer'].search([
+                ('product_id', '=', product.id),
+                ('create_date', '=', create_date),
+                ('company_id', '=', self.env.company.id)
+            ], limit=1)
+
+            if existing_valuation and existing_valuation.account_move_id:
+                _logger.warning(
+                    "Valuation and journal entry already exist for product: %s on date: %s. Skipping record.",
+                    product.name, create_date)
+                continue
+
+            elif existing_valuation:
+                _logger.warning("Valuation already exists for product: %s on date: %s. Skipping record.",
+                                product.name, create_date)
+                continue
+
+            elif self.env['account.move'].search([
+                ('ref', 'ilike', f"Stock Valuation Adjustment for {product.name}"),
+                ('date', '=', create_date),
+                ('journal_id', '=', product.categ_id.property_stock_journal.id)
+            ], limit=1):
+                _logger.warning("Account move already exists for product: %s on date: %s. Skipping record.",
+                                product.name, create_date)
+                continue
+
+            stock_moves = self.env['stock.move'].search([
+                ('product_id', '=', product.id),
+                ('location_dest_id.usage', '=', 'production'),
+                ('location_id.usage', 'in', ['internal', 'production']),
+                ('state', '=', 'done'),
+                ('date', '>=', date_start),
+                ('date', '<=', date_end),
+                ('company_id', '=', self.env.company.id),
+                ('stock_valuation_layer_ids', '=', False)
+            ])
+
+            if not stock_moves:
+                _logger.warning("No stock moves found for product: %s", product.display_name)
+                continue
+
+            total_qty = sum(stock_moves.mapped('product_uom_qty'))
+            if total_qty == 0:
+                raise UserError(_("Total quantity is zero for product: %s") % product.default_code)
+
+            valuation_layers = self.env['stock.valuation.layer'].search([
+                ('product_id', '=', product.id),
+                ('create_date', '>=', date_start),
+                ('create_date', '<=', date_end),
+                ('company_id', '=', self.env.company.id),
+                ('stock_move_id.location_id.usage', '=', 'supplier')
+            ])
+
+            if not valuation_layers:
+                # Get the last valuation layer before the start date
+                last_layer = self.env['stock.valuation.layer'].search([
                     ('product_id', '=', product.id),
-                    ('create_date', '=', create_date),
-                    ('company_id', '=', self.env.company.id)
-                ], limit=1)
-
-                if existing_valuation and existing_valuation.account_move_id:
-                    _logger.warning(
-                        "Valuation and journal entry already exist for product: %s on date: %s. Skipping record.",
-                        product.name, create_date)
-                    continue
-
-                elif existing_valuation:
-                    _logger.warning("Valuation already exists for product: %s on date: %s. Skipping record.",
-                                    product.name, create_date)
-                    continue
-
-                elif self.env['account.move'].search([
-                    ('ref', 'ilike', f"Stock Valuation Adjustment for {product.name}"),
-                    ('date', '=', create_date),
-                    ('journal_id', '=', product.categ_id.property_stock_journal.id)
-                ], limit=1):
-                    _logger.warning("Account move already exists for product: %s on date: %s. Skipping record.",
-                                    product.name, create_date)
-                    continue
-
-                stock_moves = self.env['stock.move'].search([
-                    ('product_id', '=', product.id),
-                    ('location_dest_id.usage', '=', 'production'),
-                    ('location_id.usage', '=', 'internal'),
-                    ('state', '=', 'done'),
-                    ('date', '>=', date_start),
-                    ('date', '<=', date_end),
                     ('company_id', '=', self.env.company.id),
-                    ('stock_valuation_layer_ids', '=', False)
-                ])
-                # If no matching stock_moves are found, search again with adjusted criteria
-                if not stock_moves:
-                    stock_moves = self.env['stock.move'].search([
-                        ('product_id', '=', product.id),
-                        ('location_dest_id.usage', '=', 'production'),
-                        ('location_id.usage', '=', 'production'),
-                        ('state', '=', 'done'),
-                        ('date', '>=', date_start),
-                        ('date', '<=', date_end),
-                        ('company_id', '=', self.env.company.id),
-                        ('stock_valuation_layer_ids', '=', False)
-                    ])
-                valuation_layers = self.env['stock.valuation.layer'].search([
-                    ('product_id', '=', product.id),
-                    ('create_date', '>=', date_start),
-                    ('create_date', '<=', date_end),
-                    ('company_id', '=', self.env.company.id),
-                    ('stock_move_id.location_id.usage', '=', 'supplier')  # Ensure correct field path
-                ])
-                if not valuation_layers:
-                    # Get the last valuation layer before the start date
-                    last_layer = self.env['stock.valuation.layer'].search([
-                        ('product_id', '=', product.id),
-                        ('company_id', '=', self.env.company.id),
-                    ], order='create_date desc', limit=1)
+                ], order='create_date desc', limit=1)
 
-                    if last_layer:
-                        total_qty = sum(stock_moves.mapped('product_uom_qty'))
-                        unit_cost = sum(last_layer.mapped('value'))
-                        average_unit_cost = unit_cost / total_qty if total_qty else 0
-                    else:
-                        total_qty = sum(stock_moves.mapped('product_uom_qty'))
-                        average_unit_cost = product.standard_price
-                        print(f"✅ Using last updated unit price from previous layer: {average_unit_cost}")
-                else:
-                    # Calculate the average unit cost from the layers
-                    total_qty = sum(stock_moves.mapped('product_uom_qty'))
-                    unit_cost = sum(valuation_layers.mapped('value'))
-                    average_unit_cost = unit_cost / total_qty if total_qty else 0
-                print(f"✅ Calculated Average Unit Cost: {average_unit_cost}")
+            unit_cost = sum(valuation_layers.mapped('value')) if valuation_layers else (sum(last_layer.mapped('value')) if last_layer else product.standard_price)
+            average_unit_cost = unit_cost / total_qty if total_qty else 0
+            total_value = average_unit_cost * total_qty
+
+            stock_valuation = self.env['stock.valuation.layer'].create({
+                'create_date': create_date,
+                'product_id': product.id,
+                'company_id': self.env.company.id,
+                'quantity': -1 * total_qty,
+                'unit_cost': average_unit_cost,
+                'value': -1 * total_value,
+            })
+            _logger.info("Layer created for %s", product.name)
+            self.env.cr.execute('UPDATE stock_valuation_layer SET create_date = %s WHERE id=%s',
+                                (create_date, stock_valuation.id,))
 
 
-                if total_qty == 0:
-                    raise UserError(_("Total quantity is zero for product: %s") % product.default_code)
-                total_value = average_unit_cost * total_qty
 
-                # Create stock valuation layer
-                stock_valuation = self.env['stock.valuation.layer'].create({
-                    'create_date': create_date,
-                    'product_id': product.id,
-                    'company_id': self.env.company.id,
-                    'quantity': -1 * total_qty,
-                    'unit_cost': average_unit_cost,
-                    'value': -1 * total_value,
-                })
-                self.env.cr.execute('UPDATE stock_valuation_layer SET create_date = %s WHERE id=%s',
-                                    (create_date, stock_valuation.id,))
+            category = product.categ_id
+            if not (
+                    category.property_stock_valuation_account_id and category.property_stock_account_input_categ_id and category.property_stock_journal):
+                raise UserError(_("Accounts or journal not configured for product category: %s") % category.name)
 
-                _logger.info( stock_valuation,id,"Created stock valuation for %s with Qty: %s, Unit Cost: %s", product.name, total_qty, average_unit_cost)
+            account_move = self.env['account.move'].create({
+                'journal_id': category.property_stock_journal.id,
+                'ref': _(f"Stock Valuation Adjustment for {product.name}"),
+            })
 
-                # Journal Entry Creation
-                category = product.categ_id
-                debit_account = category.property_stock_valuation_account_id
-                credit_account = category.property_stock_account_input_categ_id
-                journal = category.property_stock_journal
+            account_move.write({'line_ids': [
+                (0, 0,
+                 {'account_id': category.property_stock_account_input_categ_id.id, 'credit': total_value, 'debit': 0.0,
+                  'name': f"Stock Valuation Credit for {product.name}", 'product_id': product.id,
+                  'quantity': total_qty}),
+                (0, 0,
+                 {'account_id': category.property_stock_valuation_account_id.id, 'debit': total_value, 'credit': 0.0,
+                  'name': f"Stock Valuation Debit for {product.name}", 'product_id': product.id, 'quantity': total_qty})
+            ]})
 
-                if not debit_account or not credit_account:
-                    raise UserError(_("Accounts not configured for product category: %s") % category.name)
+            account_move.action_post()
+            stock_valuation.write(
+                {'account_move_id': account_move.id, 'description': f'Manual correction RM {reference_count} -PPTS'})
+            reference_count += 1
+            completed_count += 1
+            _logger.info("Journal Entry created and posted for %s", product.name)
 
-                account_move = self.env['account.move'].create({
-                    'journal_id': journal.id,
-                    'ref': _(f"Stock Valuation Adjustment for {product.name}"),
-                })
-
-                line_ids = [
-                    (0, 0, {
-                        'account_id': credit_account.id,
-                        'debit': 0.0,
-                        'credit': total_value,
-                        'name': _(f"Stock Valuation Credit for {product.name}"),
-                        'product_id': product.id,
-                        'quantity': total_qty,
-                    }),
-                    (0, 0, {
-                        'account_id': debit_account.id,
-                        'debit': total_value,
-                        'credit': 0.0,
-                        'name': _(f"Stock Valuation Debit for {product.name}"),
-                        'product_id': product.id,
-                        'quantity': total_qty,
-                    }),
-                ]
-
-                account_move.sudo().write({'line_ids': line_ids})
-                account_move.action_post()
-                reference = f'Manual correction RM {reference_count} -PPTS'
-                reference_count += 1
-                stock_valuation.write({'account_move_id': account_move.id, 'description': reference})
-                self.env.cr.execute('UPDATE account_move SET date = %s WHERE id=%s',
-                                     (create_date, account_move.id,))
-                # Update the date using SQL query
-
-                _logger.info("Updated stock valuation layer date for %s to %s", product.name, create_date)
+        _logger.info("Stock Valuation Import Process Completed Successfully. Total Records Processed: %s",
+                     completed_count)
 
     # def import_stock_valuation_data(self):
     #     csv_data = base64.b64decode(self.file)
