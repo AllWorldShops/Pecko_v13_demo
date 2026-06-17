2c822700fa6dddb160fbc8c44538f95808e8887f
thangaraj.m <thangaraj.murugesan@pptservices.com>
2026-06-17 11:13:30 +0530

    PO line limit issue fixed2

--- purchase_order_line_limit/models/res_company.py
+++ purchase_order_line_limit/models/res_company.py
@@ -5,11 +5,4 @@ class ResCompany(models.Model):
 
     purchase_order_line_limit = fields.Integer(string="PO Line Limit",help="Defines the maximum number of lines allowed in a purchase order. When this limit is exceeded, a new purchase order will be created.")
 
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    purchase_order_line_limit = fields.Integer(
-        related='company_id.purchase_order_line_limit',
-        readonly=False
-    )
+    
